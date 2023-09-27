40bea26233196637b7ff72b3752bc6d94c5eaae8
Veikkosuhonen <vesuvesuh@gmail.com>
2023-09-27 21:57:15 +0300

    Relations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,3 @@
 class Meal < ApplicationRecord
+  has_many :menu_items
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Exavaichemi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_job.queue_adapter = :sucker_punch
   end
 end
