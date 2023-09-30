a3899797e3cf32dd6be802fa8f08308c28349ccd
Veikkosuhonen <vesuvesuh@gmail.com>
2023-09-30 18:18:21 +0300

    style: Layout sketch

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        appear: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        appear: 'appear 0.25s ease-in-out',
+      }
     },
   },
   plugins: [
