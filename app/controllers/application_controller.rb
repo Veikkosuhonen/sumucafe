518438d995431e51a1c105902612e6479db5c267
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-03 12:05:13 +0300

    Maybe fix timezones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  around_action :set_time_zone
+
   def current_user
     return nil if session[:user_id].nil?
 
     User.find_by id: session[:user_id]
+  end
+
+  def set_time_zone(&block)
+    Time.use_zone("Helsinki", &block)
   end
 end
--- app/jobs/update_unicafe_menu_job.rb
+++ app/jobs/update_unicafe_menu_job.rb
@@ -4,38 +4,40 @@ class UpdateUnicafeMenuJob < ApplicationJob
   queue_as :default
 
   def perform
-    datafile = File.open "./test/fixtures/apiresponse.json"
+    Time.use_zone("Helsinki") {
+      datafile = File.open "./test/fixtures/apiresponse.json"
 
-    data = JSON.load datafile
+      data = JSON.load datafile
 
-    datafile.close
+      datafile.close
 
-    data.each { |restaurant_data|
-      name = restaurant_data["title"]
+      data.each { |restaurant_data|
+        name = restaurant_data["title"]
 
-      restaurant = Restaurant.find_or_create_by({
-                                                  :name => name,
-                                                })
+        restaurant = Restaurant.find_or_create_by({
+                                                    :name => name,
+                                                  })
 
-      restaurant_data["menuData"]["menus"].each { |menu_data|
-        menu_data["data"].each { |menu_item_data|
-          meal_name = menu_item_data["name"]
+        restaurant_data["menuData"]["menus"].each { |menu_data|
+          menu_data["data"].each { |menu_item_data|
+            meal_name = menu_item_data["name"]
 
 
-          meal = Meal.find_or_create_by({
-            :name => meal_name
-                                        })
+            meal = Meal.find_or_create_by({
+              :name => meal_name
+                                          })
 
-          menu_date_string = menu_data["date"]
-          menu_date_string = menu_date_string.slice(3, menu_date_string.length)
+            menu_date_string = menu_data["date"]
+            menu_date_string = menu_date_string.slice(3, menu_date_string.length)
 
-          date = Date.strptime(menu_date_string, "%d.%m.")
+            date = Date.strptime(menu_date_string, "%d.%m.")
 
-          menu_item = MenuItem.find_or_create_by({
-            :meal_id => meal.id,
-            :restaurant_id => restaurant.id,
-            :menu_date => date,
-                                                 })
+            menu_item = MenuItem.find_or_create_by({
+              :meal_id => meal.id,
+              :restaurant_id => restaurant.id,
+              :menu_date => date,
+                                                   })
+          }
         }
       }
     }
