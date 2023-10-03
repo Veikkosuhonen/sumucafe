0d0c1f83b00efd94b94dbc3346f2374b0d678996
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-03 11:45:34 +0300

    Fix menu date range for todays menu

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :menu_items
 
-  scope :with_todays_menu, -> { includes(menu_items: :meal).where(menu_items: { menu_date: Date.today..Date.tomorrow }) }
+  scope :with_todays_menu, -> { includes(menu_items: :meal).where(menu_items: { menu_date: Date.today...Date.tomorrow }) }
 end
