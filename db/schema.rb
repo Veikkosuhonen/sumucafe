a3bda6e3a926d15ee2da03075a716d03ea4b7d51
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-03 11:41:32 +0300

    Betterment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_30_161707) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_082659) do
   create_table "meals", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_30_161707) do
     t.integer "restaurant_id"
     t.integer "meal_id"
     t.date "menu_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "meal_id"
+    t.integer "user_id"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
