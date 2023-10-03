380cf9d7938f0d252aa6d5979472f8f773294915
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-03 11:25:24 +0300

    Users & sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  resources :users
   resources :menu_items
   resources :meals
   resources :restaurants
 
+  resource :session, only: [:new, :create, :destroy]
+
   root "restaurants#index"
+
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
