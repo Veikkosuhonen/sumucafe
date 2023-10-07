da52d314c73f3ea40b1cec81c567ab7ffbef1795
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-07 23:10:50 +0300

    Styleying & imgs

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,5 +5,5 @@ class Restaurant < ApplicationRecord
   validates :name, uniqueness: true
 
   scope :with_todays_menu, -> {
-    includes(:location, menu_items: :meal).where(menu_items: { menu_date: 3.day.from_now...4.day.from_now, is_canceled: false }) }
+    includes(:location, menu_items: :meal).where(menu_items: { menu_date: Date.today.beginning_of_day...Date.tomorrow.beginning_of_day, is_canceled: false }) }
 end
