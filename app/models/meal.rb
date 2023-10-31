fe5523b4076e2ac8964571c7b9cbb0fb4497fa56
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-31 18:06:24 +0200

    Show menu item scores on front page

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,9 @@ class Meal < ApplicationRecord
   has_many :menu_items
   has_many :ratings
   belongs_to :meal_type
+
+  def score
+    @score ||= ratings.average(:score).to_f.round(2)
+    @score
+  end
 end
